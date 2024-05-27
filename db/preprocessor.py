f0577f267a7b107e0803b6c634d034329e20527b
Stuart Leach <jstuartleach@gmail.com>
2024-05-27 15:58:25 -0400

    formatting

--- db/preprocessor.py
+++ db/preprocessor.py
@@ -8,15 +8,27 @@ from db import save_preprocessed_data
 
 
 def convert_bail_amount(data):
+    """
+    Convert the bail amount column to numeric and rename it.
+
+    :param data:
+    :return:
+    """
     data["first_bail_set_cash"] = pd.to_numeric(
         data["first_bail_set_cash"], errors="coerce"
     )
     data.rename(columns={"first_bail_set_cash": "bail_amount"}, inplace=True)
-    logging.info(f"First few rows of data: {data.head()}")
+    logging.info("First few rows of data: %s", data.head())
     return data
 
 
 def drop_list_columns(data):
+    """
+    Drop columns that contain list values.
+
+    :param data:
+    :return:
+    """
     columns_to_drop = [
         column
         for column in data.columns
@@ -24,13 +36,19 @@ def drop_list_columns(data):
     ]
     if columns_to_drop:
         logging.warning(
-            f"Columns {columns_to_drop} contain list values and will be dropped."
+            "Columns %s contain list values and will be dropped.", columns_to_drop
         )
         data = data.drop(columns=columns_to_drop)
     return data
 
 
 def separate_features_and_target(data):
+    """
+    Separate features and target variables.
+
+    :param data:
+    :return:
+    """
     x = data.drop(columns=["bail_amount", "bail_amount_bin"])
     y = data["bail_amount"]
     y_bin = data["bail_amount_bin"]
@@ -38,10 +56,17 @@ def separate_features_and_target(data):
 
 
 def normalize_columns(columns_to_normalize, x):
+    """
+    Normalize the specified columns in the input data.
+
+    :param columns_to_normalize:
+    :param x:
+    :return:
+    """
     for column in columns_to_normalize:
         if column in x.columns:
             x[column] = StandardScaler().fit_transform(x[[column]])
-            logging.info(f"Normalized {column} column.")
+            logging.info("Normalized %s column.", column)
     return x
 
 
@@ -66,6 +91,13 @@ class Preprocessor:
         self.scaler = StandardScaler()
 
     def preprocess_data(self, data, outputs_dir):
+        """
+        Preprocess the input data.
+
+        :param data:
+        :param outputs_dir:
+        :return:
+        """
         data = convert_bail_amount(data)
         self._create_bins(data)
         data = drop_list_columns(data)
@@ -77,11 +109,16 @@ class Preprocessor:
         return x, y, y_bin
 
     def _create_bins(self, data):
+        """
+        Create bins for the bail amount column.
+        :param data:
+        :return:
+        """
         data["bail_amount_bin"] = pd.cut(
             data["bail_amount"], bins=self.num_bins, labels=False
         )
         bin_counts = data["bail_amount_bin"].value_counts()
-        logging.info(f"Bin counts before adjustment: {bin_counts}")
+        logging.info("Bin counts before adjustment: %s", bin_counts)
         bin_count_threshold = 2
         while any(bin_counts < bin_count_threshold):
             self.num_bins -= 1
@@ -94,9 +131,15 @@ class Preprocessor:
                 data["bail_amount"], bins=self.num_bins, labels=False
             )
             bin_counts = data["bail_amount_bin"].value_counts()
-            logging.info(f"Adjusted bin counts: {bin_counts}")
+            logging.info("Adjusted bin counts: %s", bin_counts)
 
     def _encode_categorical_features(self, data):
+        """
+        Encode categorical features using the specified strategy.
+
+        :param data:
+        :return:
+        """
         if self.encoding_strategy == "label":
             for column in data.select_dtypes(include=["object"]).columns:
                 le = LabelEncoder()
@@ -111,13 +154,21 @@ class Preprocessor:
         return data
 
     def _handle_missing_values(self, x, y, y_bin):
+        """
+        Handle missing values in the input data.
+
+        :param x:
+        :param y:
+        :param y_bin:
+        :return:
+        """
         nan_count = y.isna().sum()
-        logging.info(f"Number of NaN values in bail_amount: {nan_count}")
+        logging.info("Number of NaN values in bail_amount: %s", nan_count)
         if nan_count > 0:
             x = x[~y.isna()]
             y = y[~y.isna()]
             y_bin = y_bin[~y.isna()]
-            logging.info(f"Removed {nan_count} rows with NaN values in bail_amount.")
+            logging.info("Removed %s rows with NaN values in bail_amount.", nan_count)
         x = pd.DataFrame(self.imputer.fit_transform(x), columns=x.columns)
         logging.info("Filled NaN values in features with column medians.")
         return x, y, y_bin
