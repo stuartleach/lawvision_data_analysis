d8e1958662f2943c61084e97baf1615263222fdb
Stuart Leach <jstuartleach@gmail.com>
2024-05-29 12:13:18 -0400

    Add details to GNU license

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """This is the main file that runs the application."""
 
-# <one line to give the program's name and a brief idea of what it does.>
+# Law Vision Data Analysis trains an AI model to understand which what factors play into judges' decisions when
+# setting bail amounts.
 #     Copyright (C) 2024 J. Stuart Leach, Jr.
 #
 #     This program is free software: you can redistribute it and/or modify
